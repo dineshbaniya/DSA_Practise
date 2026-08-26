b0f9392f732317091c50db4d4d6b580081588538
Dinesh Baniya <agraharid892@gmail.com>
2026-08-27 00:26:57 +0530

    Day 01: Add Two Sum solution

---
+++ Day_01/01_Two_Sum.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+        unordered_map<int, int> mp;
+
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+
+            if (mp.find(complement) != mp.end()) {
+                return {mp[complement], i};
+            }
+
+            mp[nums[i]] = i;
+        }
+
+        return {};
+    }
+};
+
+int main() {
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+
+    Solution solution;
+    vector<int> result = solution.twoSum(nums, target);
+
+    cout << "[" << result[0] << ", " << result[1] << "]" << endl;
+
+    return 0;
+}
