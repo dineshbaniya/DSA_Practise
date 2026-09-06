d04b994b878698ad92c75c1fe9bcbe753ad2ce7b
Dinesh Baniya <agraharid892@gmail.com>
2026-09-06 23:10:59 +0530

    Day 02: Add minimum price tracking

--- Day_02/01_Best_Time_to_Buy_and_Sell_Stock.cpp
+++ Day_02/01_Best_Time_to_Buy_and_Sell_Stock.cpp
@@ -49,7 +49,11 @@ using namespace std;
 
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-        
+   int maxProfit(vector<int>& prices) {
+    int minPrice = prices[0];
+
+    for (int i = 1; i < prices.size(); i++) {
+        minPrice = min(minPrice, prices[i]);
     }
+}
 };
