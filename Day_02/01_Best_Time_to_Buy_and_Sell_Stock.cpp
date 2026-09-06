2fc47d5d42c0d1b56e21b5ceda87596bf4a04cba
Dinesh Baniya <agraharid892@gmail.com>
2026-09-06 23:17:19 +0530

    Day 02: Add maximum profit calculation

--- Day_02/01_Best_Time_to_Buy_and_Sell_Stock.cpp
+++ Day_02/01_Best_Time_to_Buy_and_Sell_Stock.cpp
@@ -49,11 +49,18 @@ using namespace std;
 
 class Solution {
 public:
-   int maxProfit(vector<int>& prices) {
-    int minPrice = prices[0];
+    int maxProfit(vector<int>& prices) {
+        int minPrice = prices[0];
+        int maxProfit = 0;
 
-    for (int i = 1; i < prices.size(); i++) {
-        minPrice = min(minPrice, prices[i]);
+        for (int i = 1; i < prices.size(); i++) {
+            minPrice = min(minPrice, prices[i]);
+
+            int profit = prices[i] - minPrice;
+
+            maxProfit = max(maxProfit, profit);
+        }
+
+        return maxProfit;
     }
-}
 };
