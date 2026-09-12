1c6ef58f0af54577a77acca18b981aa107e55655
Dinesh Baniya <agraharid892@gmail.com>
2026-09-12 17:49:25 +0530

    Day 05: Add group anagrams problem statement

---
+++ DSA_Problems/05_Group_Anagrams.cpp
@@ -0,0 +1,22 @@
+/*
+Problem: Group Anagrams
+
+Given an array of strings, group the anagrams together.
+
+An anagram is a word formed by rearranging the letters of another word.
+
+Example:
+
+Input:
+["eat", "tea", "tan", "ate", "nat", "bat"]
+
+Output:
+[
+    ["eat", "tea", "ate"],
+    ["tan", "nat"],
+    ["bat"]
+]
+
+Note:
+The order of the output groups does not matter.
+*/
